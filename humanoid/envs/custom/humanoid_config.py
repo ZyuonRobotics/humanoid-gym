bdbc0cc99b26fdf2f0470596da062652b2d5e16f
frank <franktian424@qq.com>
2025-06-11 10:48:00 +0800

    add robot.xml

--- humanoid/envs/custom/humanoid_config.py
+++ humanoid/envs/custom/humanoid_config.py
@@ -55,7 +55,8 @@ class XBotLCfg(LeggedRobotCfg):
         torque_limit = 0.85
 
     class asset(LeggedRobotCfg.asset):
-        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/XBot/urdf/XBot-L.urdf'
+        # file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/XBot/urdf/XBot-L.urdf'
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/XBot/mjcf/robot.xml'
 
         name = "XBot-L"
         foot_name = "ankle_roll"
